0f2760ecb509f7aba4fc0d936c61e95d10f6afc5
HellCAT0147 <letscheckmail23@gmail.com>
2023-05-07 02:04:12 +0500

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const { mkdir, copyFile, readdir, rm } = require('fs/promises');
+const { join } = require('path');
 
+async function copyDir(folderName, outside = true, outsideFolder, outsidePath) {
+  let copyFolderName;
+  let folderPath;
+  let folderCopyPath;
+  if (outside) {
+    copyFolderName = folderName + '-copy';
+    folderCopyPath = join(__dirname, copyFolderName);
+  } else {
+    folderCopyPath = join(outsidePath, folderName);
+    folderName = join(outsideFolder, folderName);
+    copyFolderName = folderName;
+  }
+  folderPath = join(__dirname, folderName);
+  try {
+    mkdir(folderCopyPath, { recursive: true });
+    const options = {
+      encoding: 'utf-8',
+      withFileTypes: true
+    };
+    const smths = await readdir(folderPath, options);
+    for (const smth of smths) {
+      if (smth.isFile()) {
+        await copyFile(join(folderPath, smth.name), join(folderCopyPath, smth.name));
+      } else {
+        await copyDir(smth.name, false, folderName, folderCopyPath);
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const folderName = 'files';
+const folderCopyPath = join(__dirname, 'files-copy');
+
+async function check() {
+  if (!(await rm(folderCopyPath, { recursive: true, force: true }))) {
+    copyDir(folderName);
+  }
+}
+check();
